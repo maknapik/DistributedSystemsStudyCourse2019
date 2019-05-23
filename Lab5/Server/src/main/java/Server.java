dfecca66f6d5d930acfb4103fd92b2cf7e4b288b
maknapik <m.knapik6@gmail.com>
2019-05-23 15:18:43 +0200

    Removed unused import

--- Lab5/Server/src/main/java/Server.java
+++ Lab5/Server/src/main/java/Server.java
@@ -1,5 +1,4 @@
 import actor.ServerActor;
-import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import com.typesafe.config.ConfigFactory;
@@ -13,15 +12,15 @@ public class Server {
         this.databaseService = databaseService;
     }
 
-    private void run() {
-        final ActorSystem actorSystem = ActorSystem.create("ServerSystem", ConfigFactory.load());
-        actorSystem.actorOf(Props.create(ServerActor.class, databaseService), "Server");
-    }
-
     public static void main(String[] args) {
         Server server = new Server(new DatabaseService());
 
         server.run();
+    }
+
+    private void run() {
+        final ActorSystem actorSystem = ActorSystem.create("ServerSystem", ConfigFactory.load());
+        actorSystem.actorOf(Props.create(ServerActor.class, databaseService), "Server");
     }
 
 }
